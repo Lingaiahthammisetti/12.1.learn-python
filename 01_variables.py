4f4abab84461cc33cbc3ce6c53fdc71a54149e0d
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-11-26 22:31:29 +0530

    python learning

---
+++ 01_variables.py
@@ -0,0 +1,9 @@
+#This is a single line comment.
+"""
+This is a multi-line comment.
+"""
+#Define a variable
+a = 42 # This is a integer variable.
+
+#Print the value that is scored inside the variable.
+print(a)
