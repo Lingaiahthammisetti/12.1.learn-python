a08def9602a4e30fac8370fd18ae761708e7c7fa
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-11-26 22:40:50 +0530

    commit variables chagnes

--- 01_variables.py
+++ 01_variables.py
@@ -7,3 +7,25 @@ a = 42 # This is a integer variable.
 
 #Print the value that is scored inside the variable.
 print(a)
+
+#Float value
+b = 42.345
+print(a)
+
+#Boolean
+c =True #true
+
+#Strings
+d ='This is a string'
+e ="This is alos a string"
+f ="""
+ This is a multi-line string
+"""
+
+#Today's weather is nice
+d = 'Today\s weather is nice'
+print(d)
+
+# d = "Today's weather is nice"
+# print(d)
+
