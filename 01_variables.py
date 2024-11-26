dcc615fb06a0eb6149148c00c115d78de69a8e65
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-11-26 23:02:52 +0530

    commit variable changes.

--- 01_variables.py
+++ 01_variables.py
@@ -50,3 +50,70 @@ print(test_set)
 print(type(test_dict))
 print(type(print))
 
+# Operations
+# Add
+# Sub
+# Multi
+# Divide
+# Integer division
+# Modulo divion
+
+# Add
+a = 42
+b = 45.32
+c = a + b
+print(c)
+
+d = a - b
+print(d)
+
+f = 12
+g = 3
+h = f/g
+print(h, type(h))
+
+#Integer division
+h = f//g
+print(h)
+
+#Modulo division
+i  = f%g
+print(i)
+
+#Addition
+a = "42"
+b = "43"
+print(a+b) #concatenation
+
+# Power
+a = 10
+print(a**2) #a2
+
+# Comparision operators
+a = 10
+b = 20
+res = a > b
+res_1 = a < b
+res_2 = a != b
+res_3 = a == b
+print(res, res_1, res_2, res_3)
+
+# Logical operators
+# AND, OR, NOT
+a = True
+b = False
+
+res = a and b
+res_1 = a or b
+res_2 = not a
+res_3 = not b
+print(res, res_1, res_2, res_3)
+
+
+
+
+
+
+
+
+
