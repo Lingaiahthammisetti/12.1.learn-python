07758b5cf5e4821c717f930ad5ea1c42a223ca6b
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-11-30 16:05:27 +0530

    learning python

--- 11_functions.py
+++ 11_functions.py
@@ -8,17 +8,17 @@ def sample_func():
 sample_func()
 
 # #****************************************
-# def add(num_1, num_2):
-#     """
-#     This function performs addition of 2 numbers
-#     """
-#     res = num_1 + num_2
-#     return res
+def add(num_1, num_2):
+    """
+    This function performs addition of 2 numbers
+    """
+    res = num_1 + num_2
+    return res
 
-# # Call the function
-# # res = add(1, 2)
-# res = add(num_2=1, num_1=2)
-# print(res)
+# Call the function
+# res = add(1, 2)
+res = add(num_2=1, num_1=2)
+print(res)
 
 # help(add)
 # #****************************************
