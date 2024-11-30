59646db048a3ede00870d019aa3b5afb19b8cc4b
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-11-30 15:55:49 +0530

    learning python

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -1,6 +1,6 @@
 # Check whether the given number is greater than 10 or not
-
 user_input = int(input("Enter a number:"))
+# type cast into integer
 user_input = int(user_input)
 
 if user_input > 10:
@@ -9,7 +9,6 @@ else:
     print("User input value is less than or equal 10")
 
 # Check whether the given number is equal to 10 or greater than 10 or less than 10 or something else
-
 # Method 1: Nested if-else
 if user_input == 10:
     print("Right on target")
@@ -27,7 +26,6 @@ elif user_input > 10:
     print("User input value is greater than 10")
 else:
     print("User input value is less than 10")
-
 
 # Exception handling: try - except
 try:
--- 11_functions.py
+++ 11_functions.py
@@ -1,64 +1,68 @@
-# def sample_func():
-#     print("This is a sample function")
 
-# # Call the function
-# sample_func()
 
-# def add(num_1, num_2):
-#     """
-#     This function performs addition of 2 numbers
-#     """
-#     res = num_1 + num_2
-#     return res
+#****************************************
+def sample_func():
+    print("This is a sample function")
 
-# # Call the function
-# # res = add(1, 2)
-# res = add(num_2=1, num_1=2)
-# print(res)
+# Call the function
+sample_func()
 
-# help(add)
-
-# def add(num_1, num_2, num_3=10):
-#     """
-#     This function performs addition of 2 numbers
-#     """
-#     res = num_1 + num_2 + num_3
-#     return res
+#****************************************
+def add(num_1, num_2):
+    """
+    This function performs addition of 2 numbers
+    """
+    res = num_1 + num_2
+    return res
 
 # Call the function
 # res = add(1, 2)
-# res = add(num_2=1, num_1=2)
-# print(res)
+res = add(num_2=1, num_1=2)
+print(res)
+
+help(add)
+#****************************************
+def add(num_1, num_2, num_3=10):
+    """
+    This function performs addition of 2 numbers
+    """
+    res = num_1 + num_2 + num_3
+    return res
+
+Call the function
+res = add(1, 2)
+res = add(num_2=1, num_1=2)
+print(res)
 
 # One user enters 10 numbers and another user enters 100 numbers. Define your function to handle this situation
 # A: Variable length arguments
+#****************************************
+def add(*nums):
+    """
+    This function performs addition of 2 numbers
+    """
+    res = sum(nums)
+    return res
 
-# def add(*nums):
-#     """
-#     This function performs addition of 2 numbers
-#     """
-#     res = sum(nums)
-#     return res
+res = add(1, 2, 4)
+print(res)
 
-# res = add(1, 2, 4)
-# print(res)
+res = add(1, 2, 4, 5, 6)
+print(res)
+#****************************************
+def add(num1, num2, *args):
+    """
+    This function performs addition of 2 numbers
+    """
+    res = num1 + num2 + sum(args)
+    return res
 
-# res = add(1, 2, 4, 5, 6)
-# print(res)
+res = add(1, 2, 4, 5)
+print(res)
 
-# def add(num1, num2, *args):
-#     """
-#     This function performs addition of 2 numbers
-#     """
-#     res = num1 + num2 + sum(args)
-#     return res
-
-# res = add(1, 2, 4, 5)
-# print(res)
-
-# res = add(1, 2, 4, 5, 6)
-# print(res)
-
+res = add(1, 2, 4, 5, 6)
+print(res)
+#****************************************
 def add(*args, **kwargs):
     """
     This function performs addition of 2 numbers
