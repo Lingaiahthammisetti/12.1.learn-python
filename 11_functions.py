c6979c5fc70a7b06c49360144ba86fc14e642f17
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-11-30 16:22:51 +0530

    learning python

--- 11_functions.py
+++ 11_functions.py
@@ -74,12 +74,58 @@ print(res)
 res = add(1, 2, 4, num1=5, num2=6)
 print(res)
 
+def add(*args, **kwargs):
+    """
+    This function performs addition of 2 numbers
+    """
+    print(args, kwargs)
+
+res = add(1, 2, 4)
+print(res)
+print("=========================================")
+def myFun(*argv):
+    for arg in argv:
+        print(arg)
+myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
+
+print("=========================================")
+def myFun(arg1, *argv):
+    print("First argument :", arg1)
+    for arg in argv:
+        print("Next argument through *argv :", arg)
+
+myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
+
+print("=========================================")
+
+def myFun(**kwargs):
+    for key, value in kwargs.items():
+        print("%s == %s" % (key, value))
+
+# Driver code
+myFun(first='Geeks', mid='for', last='Geeks')
+print("=========================================")
+def myFun(arg1, arg2, arg3):
+    print("arg1:", arg1)
+    print("arg2:", arg2)
+    print("arg3:", arg3)
+
+# Now we can use *args or **kwargs to
+# pass arguments to this function :
+args = ("Geeks", "for", "Geeks")
+myFun(*args)
+
+kwargs = {"arg1": "Geeks", "arg2": "for", "arg3": "Geeks"}
+myFun(**kwargs)
+
+
+
 # # map
 # # filter
 # #****************************************
 # # Lambda: inline function
 
-# add_numbers = lambda num1, num2: num1 + num2
-# res = add_numbers(1, 2)
-# print(res)
+add_numbers = lambda num1, num2: num1 + num2
+res = add_numbers(1, 2)
+print(res)
 
